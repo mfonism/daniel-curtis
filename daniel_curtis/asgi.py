92b6f4588012278eebc53454d8ba1b22dda259a5
Mfon Eti-mfon <mfonetimfon@gmail.com>
2020-08-14 15:09:07 +0100

    Initialize project -- use custom user model

---
+++ daniel_curtis/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for daniel_curtis project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'daniel_curtis.settings')
+
+application = get_asgi_application()
