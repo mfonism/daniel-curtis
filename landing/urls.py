310f8a77374cc0263783a86e74a31fe04bfb9670
Mfon Eti-mfon <mfonetimfon@gmail.com>
2020-08-28 01:30:16 +0100

    Handle redirect on successfully submitting feedback in parked landing

--- landing/urls.py
+++ landing/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from .views import ParkedView
+from .views import ParkedView, SubmitSuccessView
 
 urlpatterns = [
     path("", ParkedView.as_view(), name="parked_landing"),
+    path(
+        "submitted", SubmitSuccessView.as_view(), name="parked_landing_submit_success"
+    ),
 ]
--- landing/views.py
+++ landing/views.py
@@ -1,16 +1,17 @@
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, TemplateView
 
 from .models import WantToSee
 
 
 class ParkedView(CreateView):
-    """
-    Template view for landing page of the parked site.
-    """
-
     http_method_names = ["get", "post"]
     template_name = "landing/parked.html"
     model = WantToSee
     fields = ["fullname", "email", "message"]
-    success_url = reverse_lazy("parked_landing")
+    success_url = reverse_lazy("parked_landing_submit_success")
+
+
+class SubmitSuccessView(TemplateView):
+    http_method_names = ["get"]
+    template_name = "landing/submit-success.html"
